4d6c70eccbfc01a14818518a055bef1c0f82db91
Futri <smfurdyna@ail.com>
2022-08-11 23:33:20 +0200

    All task without Optional

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 const amountOfGrid = 4;
 
 const container = document.querySelector('.container');
+const resetButt = document.querySelector('button');
 
 const createGrid = (amountOfGrid) => {
-    for(let i = 0; i<amountOfGrid; i++){
+    const wrap = document.createElement('div');
+    wrap.classList.add('wrap');
+
+    for (let i = 0; i < amountOfGrid; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row') // adding to a class so I can style the above line.
 
-        for(let j = 0; j<amountOfGrid; j++){
+        for (let j = 0; j < amountOfGrid; j++) {
             const widthHeight = 960 / amountOfGrid;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
@@ -20,8 +24,19 @@ const createGrid = (amountOfGrid) => {
             })
             row.appendChild(gridBox);
         }
-        container.appendChild(row);
+        wrap.appendChild(row);
     }
+    container.appendChild(wrap);
 }
 
 createGrid(amountOfGrid);
+
+resetButt.addEventListener('click', () => {
+    let userSize = Number(prompt('Gib dimentions: '));
+    while (userSize > 100) {
+        userSize = Number(prompt('Gib dimentions: '));
+    }
+    const wrap = document.querySelector('.wrap');
+    wrap.remove();
+    createGrid(userSize);
+})
